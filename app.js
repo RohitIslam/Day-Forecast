79bba1bf66bbba05351fda32962a577b63b2ffc3
Rohit Islam <air.rohit999@gmail.com>
2019-08-31 20:06:49 +0000

    callback chaining

--- app.js
+++ app.js
@@ -2,25 +2,21 @@ const request = require("request");
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-// const url =
-//   "https://api.darksky.net/forecast/0600da0467cac24d99d77841bd89f98e/37.8267,-122.4233?units=si";
+const address = process.argv[2];
 
-// request({ url: url, json: true }, (error, response) => {
-//   if (error) {
-//     console.log("Unable to connect");
-//   } else if (response.body.error) {
-//     console.log(response.body.error);
-//   } else {
-//     console.log(response.body.currently);
-//   }
-// });
+geocode(address, (error, data) => {
+  if (error) {
+    return console.log("Error", error);
+  }
 
-forecast(37.8267, -122.4233, (error, response) => {
-  console.log("Error", error);
-  console.log("Response", response);
-});
-
-geocode("Bangladesh", (error, response) => {
-  console.log("Error", error);
-  console.log("Response", response);
+  forecast(data.latitude, data.longitude, (error, forecastData) => {
+    if (error) {
+      return console.log("Error", error);
+    }
+    console.log(`Location: ${data.location}`);
+    console.log(`Latitude: ${data.latitude}`);
+    console.log(`Longitude: ${data.longitude}`);
+    console.log(`Rain Probability: ${forecastData.precipProbability}%`);
+    console.log(`Temperature: ${forecastData.temperature} degree`);
+  });
 });
