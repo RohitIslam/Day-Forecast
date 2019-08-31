737196e5faca80bb0e33cc307976370b64ffda96
Rohit Islam <air.rohit999@gmail.com>
2019-08-31 19:12:02 +0000

    callback function setup for geocode, utils created

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const request = require("request");
+const geocode = require("./utils/geocode");
 
 // const url =
 //   "https://api.darksky.net/forecast/0600da0467cac24d99d77841bd89f98e/37.8267,-122.4233?units=si";
@@ -13,17 +14,22 @@ const request = require("request");
 //   }
 // });
 
-const geocodeUrl =
-  "https://api.mapbox.com/geocoding/v5/mapbox.places/Bangladesh.json?access_token=pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImNqNnM2YmFoNzAwcTMzM214NTB1NHdwbnoifQ.Or19S7KmYPHW8YjRz82v6g&cachebuster=1567266180009&autocomplete=true&types=country";
+// const geocodeUrl =
+//   "https://api.mapbox.com/geocoding/v5/mapbox.places/Bangladesh.json?access_token=pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImNqNnM2YmFoNzAwcTMzM214NTB1NHdwbnoifQ.Or19S7KmYPHW8YjRz82v6g&cachebuster=1567266180009&autocomplete=true&types=country";
 
-request({ url: geocodeUrl, json: true }, (error, response) => {
-  if (error) {
-    console.log("Unable to connect");
-  } else if (response.body.features.length === 0) {
-    console.log("Unable to find location");
-  } else {
-    const latitude = response.body.features[0].center[1];
-    const longitude = response.body.features[0].center[0];
-    console.log(latitude, longitude);
-  }
+// request({ url: geocodeUrl, json: true }, (error, response) => {
+//   if (error) {
+//     console.log("Unable to connect");
+//   } else if (response.body.features.length === 0) {
+//     console.log("Unable to find location");
+//   } else {
+//     const latitude = response.body.features[0].center[1];
+//     const longitude = response.body.features[0].center[0];
+//     console.log(latitude, longitude);
+//   }
+// });
+
+geocode("Bangladesh", (error, response) => {
+  console.log("Error", error);
+  console.log("Response", response);
 });
---
+++ utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require("request");
+
+const geocode = (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    address +
+    ".json?access_token=pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImNqNnM2YmFoNzAwcTMzM214NTB1NHdwbnoifQ.Or19S7KmYPHW8YjRz82v6g&cachebuster=1567266180009&autocomplete=true&types=country";
+
+  request({ uri: url, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect", undefined);
+    } else if (response.body.features.length === 0) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(undefined, {
+        latitude: response.body.features[0].center[1],
+        longitude: response.body.features[0].center[0],
+        location: response.body.features[0].place_name
+      });
+    }
+  });
+};
+
+module.exports = geocode;
