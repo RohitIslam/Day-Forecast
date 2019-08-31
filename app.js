a436a58e015d06f0c6e81030df8aba375aaf8663
Rohit Islam <air.rohit999@gmail.com>
2019-08-31 20:10:16 +0000

    error handaled for address input

--- app.js
+++ app.js
@@ -4,19 +4,23 @@ const forecast = require("./utils/forecast");
 
 const address = process.argv[2];
 
-geocode(address, (error, data) => {
-  if (error) {
-    return console.log("Error", error);
-  }
-
-  forecast(data.latitude, data.longitude, (error, forecastData) => {
+if (!address) {
+  return console.log("Please provide an address");
+} else {
+  geocode(address, (error, data) => {
     if (error) {
       return console.log("Error", error);
     }
-    console.log(`Location: ${data.location}`);
-    console.log(`Latitude: ${data.latitude}`);
-    console.log(`Longitude: ${data.longitude}`);
-    console.log(`Rain Probability: ${forecastData.precipProbability}%`);
-    console.log(`Temperature: ${forecastData.temperature} degree`);
+
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log("Error", error);
+      }
+      console.log(`Location: ${data.location}`);
+      console.log(`Latitude: ${data.latitude}`);
+      console.log(`Longitude: ${data.longitude}`);
+      console.log(`Rain Probability: ${forecastData.precipProbability}%`);
+      console.log(`Temperature: ${forecastData.temperature} degree`);
+    });
   });
-});
+}
