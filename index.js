61b8d757eff28ba0ec7e3281f6f683613580b683
Rohit Islam <air.rohit999@gmail.com>
2019-09-01 12:59:09 +0000

    hbs used for dynamic templeting, html files converted into hbs files

--- index.js
+++ index.js
@@ -2,11 +2,30 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-console.log(__filename);
-console.log(__dirname);
-console.log(path.join(__dirname, "/public"));
-
 app.use(express.static(path.join(__dirname, "/public")));
+
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Forecast The Day",
+    name: "Rohit"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "Forecast About",
+    name: "Rohit"
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Forecast Help",
+    name: "Rohit"
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send("Express Weather");
