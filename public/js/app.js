2ae0b542b31974081f42216b83eeb4ea56fe37a9
Rohit Islam <air.rohit999@gmail.com>
2019-09-02 06:16:01 +0000

    fetched data for the client or user from server

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,25 @@
-console.log("client side js");
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+
+weatherForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const location = search.value;
+
+  fetch(`http://localhost:5000/weather?address=${location}`)
+    .then(response => {
+      response
+        .json()
+        .then(data => {
+          if (data.error) {
+            return console.log(data.error);
+          }
+          console.log(data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
